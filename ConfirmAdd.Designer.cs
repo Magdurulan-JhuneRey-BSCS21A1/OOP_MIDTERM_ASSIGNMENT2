a68cf28b1b183a75456eb497c1462f70bbcbb152
Jhune Rey E. Magdurulan <c2014-23@itmlyceumalabang.onmicrosoft.com>
2024-09-27 21:14:27 +0800

    close

--- ConfirmAdd.Designer.cs
+++ ConfirmAdd.Designer.cs
@@ -61,6 +61,7 @@
             button2.TabIndex = 2;
             button2.Text = "No";
             button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
             // 
             // ConfirmAdd
             // 
